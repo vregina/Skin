5938434ade7fd38027cbaf1f5c9f636e0c922265
vinioliveirasilva <vinioliveirasilva@hotmail.com>
2020-10-05 15:59:47 -0300

    Revisão de layout home

--- app/src/main/java/com/example/skin/home/view/HomeFragment.kt
+++ app/src/main/java/com/example/skin/home/view/HomeFragment.kt
@@ -5,10 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.example.skin.R
 
 class HomeFragment : Fragment() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
